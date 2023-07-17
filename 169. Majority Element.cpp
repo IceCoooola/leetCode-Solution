492c60ef2f2f7541dbf51135d00448ce41e7f8f6
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-07-17 13:52:00 -0700

    Update 169. Majority Element.cpp

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums)
-    {
-        map<int, int> count;
-        for(int i = 0; i < nums.size(); i++)
+    int majorityElement(vector<int>& nums) {
+        map<int, int> mp;
+        int sz = nums.size();
+        for(int i = 0; i < sz;i++)
         {
-            count[nums[i]]++;
-            if(count[nums[i]] > nums.size() /2)
-            return nums[i];
+            if(++mp[nums[i]] > sz / 2)
+                return nums[i]; 
         }
-        return -1;
+        return 0;
     }
 };
+
+
+//class Solution {
+//public:
+//    int majorityElement(vector<int>& nums)
+//    {
+//        map<int, int> count;
+//        for(int i = 0; i < nums.size(); i++)
+//        {
+//            count[nums[i]]++;
+//            if(count[nums[i]] > nums.size() /2)
+//            return nums[i];
+//        }
+//        return -1;
+//    }
+//};
