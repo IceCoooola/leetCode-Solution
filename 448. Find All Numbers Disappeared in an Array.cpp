ec6ad65493fe71e0a5d6a60e91a07466cebf89e4
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-07-22 18:28:48 -0700

    Update 448. Find All Numbers Disappeared in an Array.cpp

--- 448. Find All Numbers Disappeared in an Array.cpp
+++ 448. Find All Numbers Disappeared in an Array.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        set<int> st;
         vector<int> ret;
-        int n = nums.size();
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < nums.size(); i++)
         {
-            st.insert(nums[i]);
+            int index = abs(nums[i]) - 1;
+            if(nums[index] > 0)
+                nums[index] = -1 * nums[index];
         }
-        auto it = st.begin();
-        for(int i = 1; i <= n; i++)
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(*it != i)
-            {
-                ret.push_back(i);
-            }
-            else
-            {
-                ++it;
-            }
+            if(nums[i] > 0)
+                ret.push_back(i+1);
         }
         return ret;
     }
 };
+
+//class Solution {
+//public:
+//    vector<int> findDisappearedNumbers(vector<int>& nums) {
+//        set<int> st;
+  //      vector<int> ret;
+    //    int n = nums.size();
+      //  for(int i = 0; i < n; i++)
+        //{
+          //  st.insert(nums[i]);
+        //}
+   //     auto it = st.begin();
+   //     for(int i = 1; i <= n; i++)
+    //    {
+    //        if(*it != i)
+      //      {
+      //          ret.push_back(i);
+      //      }
+       //     else
+           // {
+          //      ++it;
+        //    }
+      //  }
+    //    return ret;
+  //  }
+//};
