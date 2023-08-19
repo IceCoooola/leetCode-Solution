48b4e333ee1cd5f061427bdcafbbf2963703eee9
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-19 16:03:49 -0700

    Update 122. Best Time to Buy and Sell Stock II.cpp

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        priority_queue<int> heap;
         int buy = 0;
-        int sell = 0;
+        int profit = 0;
         for(int i = 1; i < prices.size(); i++)
         {
-            if(prices[i] < prices[i-1])
+            if(prices[i] > prices[i-1])
             {
-                heap.push(prices[sell] - prices[buy]);
-                sell = i;
-                buy = i;
+                profit += prices[i] - prices[i-1];
             }
-            else
-            {
-                if(prices[i] - prices[buy] >= prices[sell] - prices[buy])
-                {
-                    sell = i;
-                }
-            }
-        }
-        if(sell == prices.size()-1)
-        {
-            heap.push(prices[sell] - prices[buy]);
-        }
-
-        int profit = 0;
-        while(!heap.empty())
-        {
-            profit+= heap.top();
-            heap.pop();
         }
         return profit;
     }
 };
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         priority_queue<int> heap;
+//         int buy = 0;
+//         int sell = 0;
+//         for(int i = 1; i < prices.size(); i++)
+//         {
+//             if(prices[i] < prices[i-1])
+//             {
+//                 heap.push(prices[sell] - prices[buy]);
+//                 sell = i;
+//                 buy = i;
+//             }
+//             else
+//             {
+//                 if(prices[i] - prices[buy] >= prices[sell] - prices[buy])
+//                 {
+//                     sell = i;
+//                 }
+//             }
+//         }
+//         if(sell == prices.size()-1)
+//         {
+//             heap.push(prices[sell] - prices[buy]);
+//         }
+
+//         int profit = 0;
+//         while(!heap.empty())
+//         {
+//             profit+= heap.top();
+//             heap.pop();
+//         }
+//         return profit;
+//     }
+// };
