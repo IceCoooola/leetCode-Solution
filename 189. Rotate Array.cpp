6779613ec607b6db715a1c01e06a1189f8250a52
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-19 15:03:28 -0700

    Update 189. Rotate Array.cpp

--- 189. Rotate Array.cpp
+++ 189. Rotate Array.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k %= nums.size();
-        vector<int> tmp(nums);
-        tmp.insert(tmp.end(), nums.begin(), nums.end());
-        int t = nums.size() - k;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            nums[i] = tmp[i+t];
-        }
+        k%= nums.size();
+        reverse(nums.begin(), nums.end() - k);
+        reverse(nums.end()-k, nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         k %= nums.size();
+//         vector<int> tmp(nums);
+//         tmp.insert(tmp.end(), nums.begin(), nums.end());
+//         int t = nums.size() - k;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             nums[i] = tmp[i+t];
+//         }
+//     }
+// };
