6754391fd45bd185ad46407ba7d44385a8cf714d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-18 20:40:31 -0700

    Update 26. Remove Duplicates from Sorted Array.cpp

--- 26. Remove Duplicates from Sorted Array.cpp
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0;
-        int j = 1;
-        while(j < nums.size())
+        if(nums.size() == 0)
         {
-            if(nums[i] != nums[j])
+            return 0;
+        }
+        int left = 0;
+        int right = 1;
+        while(right < nums.size())
+        {
+            if(nums[right] == nums[left])
             {
-                nums[i+1] = nums[j];
-                i++;
-                j++;
+                right++;
             }
-            else
-            {
-                j++;
+            else{
+                nums[++left] = nums[right++];
             }
         }
-        return i+1;
+        return left+1;
     }
 };
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int i = 0;
+//         int j = 1;
+//         while(j < nums.size())
+//         {
+//             if(nums[i] != nums[j])
+//             {
+//                 nums[i+1] = nums[j];
+//                 i++;
+//                 j++;
+//             }
+//             else
+//             {
+//                 j++;
+//             }
+//         }
+//         return i+1;
+//     }
+// };
