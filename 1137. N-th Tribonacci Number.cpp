b4c9a1bab9169a54e3af7068a6444c23b25ad68d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-19 17:35:19 -0700

    Update 1137. N-th Tribonacci Number.cpp

--- 1137. N-th Tribonacci Number.cpp
+++ 1137. N-th Tribonacci Number.cpp
@@ -5,13 +5,34 @@ public:
             return 0;
         if(n == 1 || n == 2)
             return 1;
-        vector<int> dp(n+1);
-        dp[0] = 0;
-        dp[1] = dp[2] = 1;
-        for(int i = 3; i <= n; i++)
+        int cur = 1;
+        int prev = 1;
+        int pprev = 0;
+        while(n >= 3)
         {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            int tmp = cur + prev + pprev;
+            pprev = prev;
+            prev = cur;
+            cur = tmp;
+            n--;
         }
-        return dp[n];
+        return cur;
     }
 };
+// class Solution {
+// public:
+//     int tribonacci(int n) {
+//         if(n == 0)
+//             return 0;
+//         if(n == 1 || n == 2)
+//             return 1;
+//         vector<int> dp(n+1);
+//         dp[0] = 0;
+//         dp[1] = dp[2] = 1;
+//         for(int i = 3; i <= n; i++)
+//         {
+//             dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+//         }
+//         return dp[n];
+//     }
+// };
