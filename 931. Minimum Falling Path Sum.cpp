82f7dd6755a997733c8d4f38c62678ce47d18c3f
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-20 17:51:09 -0700

    Create 931. Minimum Falling Path Sum.cpp

---
+++ 931. Minimum Falling Path Sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        if(matrix.size() == 1 && matrix[0].size() == 1)
+            return matrix[0][0];
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n));
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(j == 0)
+                {
+                    dp[i][j] = matrix[i-1][j] + min(dp[i-1][0], dp[i-1][1]);
+                }
+                else if(j == n - 1)
+                {
+                    dp[i][j] = matrix[i-1][j] + min(dp[i-1][n-1], dp[i-1][n-2]);
+                }
+                else
+                {
+                    dp[i][j] = matrix[i-1][j] + min(min(dp[i-1][j], dp[i-1][j-1]), dp[i-1][j+1]);
+                }
+            }
+        }
+        int ret = dp[m][0];
+        for(int j = 0; j < n; j++)
+        {
+            if(dp[m][j] < ret)
+                ret = dp[m][j];
+        }
+        return ret;
+    }
+};
