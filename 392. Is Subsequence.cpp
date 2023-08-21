de45c62fc69e51ab9087de7c9750a3e263573ab8
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-21 14:48:57 -0700

    Update 392. Is Subsequence.cpp

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.length() == 0)
-            return true;
         int idx = 0;
-        for(int i = 0; i < s.length(); i++)
+        for(int i = 0; i < t.length() && idx < s.length();i++)
         {
-            while(idx < t.length() && t[idx] != s[i])   idx++;
-            idx++;
+            if(s[idx] == t[i])
+                idx++;
         }
-        if(idx > t.size())
-            return false;
-        return true;
+        return idx == s.length();
     }
-    
 };
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         if(s.length() == 0)
+//             return true;
+//         int idx = 0;
+//         for(int i = 0; i < s.length(); i++)
+//         {
+//             while(idx < t.length() && t[idx] != s[i])   idx++;
+//             idx++;
+//         }
+//         if(idx > t.size())
+//             return false;
+//         return true;
+//     }
+    
+// };
