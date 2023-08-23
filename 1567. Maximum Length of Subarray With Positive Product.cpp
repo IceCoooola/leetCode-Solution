93d4ff7430c7ef3e71fcf61da91a2e14e7c0e4d1
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-23 10:27:37 -0700

    Create 1567. Maximum Length of Subarray With Positive Product.cpp

---
+++ 1567. Maximum Length of Subarray With Positive Product.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int sz = nums.size();
+        vector<int> pos(sz);
+        vector<int> neg(sz);
+        int maxLen = 0;
+        if(nums[0] > 0)
+        {    
+            pos[0] = 1;
+            maxLen = 1;
+        }
+        else if(nums[0] < 0)
+            neg[0] = 1;
+        for(int i = 1; i < sz; i++)
+        {
+            if(nums[i] < 0)
+            {   
+                pos[i] = (neg[i-1] == 0 ? 0 : neg[i-1] + 1);
+                neg[i] = pos[i-1] + 1;
+            }
+            else if(nums[i] > 0)
+            {
+                pos[i] = pos[i-1] + 1;
+                neg[i] = (neg[i-1] == 0? 0 : neg[i-1] + 1);
+            }
+            else
+            {
+                pos[i] = neg[i] = 0;
+            }
+            if(pos[i] > maxLen)
+                maxLen = pos[i];
+        }
+        return maxLen;
+    }
+};
