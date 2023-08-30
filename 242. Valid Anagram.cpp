e727468cd8926a457177b98acc3930664179a675
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-29 22:08:35 -0700

    Update 242. Valid Anagram.cpp

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!= t.size())
+        if(s.length() != t.length())
             return false;
-        map<char, int> compare;
-        for(int i = 0; i < s.size(); ++i)
+        map<char, int> mp;
+        int sz = s.length();
+        for(int i = 0; i < sz; i++)
         {
-            compare[s[i]]++;
+            mp[s[i]]++;
+            mp[t[i]]--;
         }
-        for(int i = 0; i < t.size(); ++i)
-        {
-            compare[t[i]]--;
-        }
-        for(auto e: compare)
+        for(auto e: mp)
         {
             if(e.second != 0)
-              return false;
+                return false;
         }
         return true;
     }
 };
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size()!= t.size())
+//             return false;
+//         map<char, int> compare;
+//         for(int i = 0; i < s.size(); ++i)
+//         {
+//             compare[s[i]]++;
+//         }
+//         for(int i = 0; i < t.size(); ++i)
+//         {
+//             compare[t[i]]--;
+//         }
+//         for(auto e: compare)
+//         {
+//             if(e.second != 0)
+//               return false;
+//         }
+//         return true;
+//     }
+// };
