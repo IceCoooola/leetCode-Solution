7c3416293fc68200c83f87ff75465c2335481b74
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-31 22:11:47 -0700

    Update 226. Invert Binary Tree.cpp

--- 226. Invert Binary Tree.cpp
+++ 226. Invert Binary Tree.cpp
@@ -2,25 +2,49 @@
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if(!root)
+            return nullptr;
+        TreeNode* tmpLeft = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(tmpLeft);
+        return root;
+    }
+};
 
-void _invertTree(struct TreeNode* root)
- {
-    if(root == NULL)
-        return;
-    if(root->left == NULL && root->right == NULL)
-        return;
-    struct TreeNode* temp = root->left;
-    root->left = root->right;
-    root->right = temp;
-    _invertTree(root->left);
-    _invertTree(root->right);
- }
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     struct TreeNode *left;
+//  *     struct TreeNode *right;
+//  * };
+//  */
 
-struct TreeNode* invertTree(struct TreeNode* root){
-    _invertTree(root);
-    return root;
-}
+// void _invertTree(struct TreeNode* root)
+//  {
+//     if(root == NULL)
+//         return;
+//     if(root->left == NULL && root->right == NULL)
+//         return;
+//     struct TreeNode* temp = root->left;
+//     root->left = root->right;
+//     root->right = temp;
+//     _invertTree(root->left);
+//     _invertTree(root->right);
+//  }
+
+// struct TreeNode* invertTree(struct TreeNode* root){
+//     _invertTree(root);
+//     return root;
+// }
