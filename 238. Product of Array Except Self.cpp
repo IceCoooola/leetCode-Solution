2057cb9976868d4ac4464bc14e446ad707ded345
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-09-03 15:54:02 -0700

    Update 238. Product of Array Except Self.cpp

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -1,46 +1,65 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1;
-        int count = 0;
-        vector<int> ret;
-        for(int i = 0; i < nums.size(); i++)
+        int sz = nums.size();
+        vector<int> productbefore(sz, 1);
+        vector<int> productafter(sz, 1);
+        vector<int> ret(sz);
+        for(int i = 1; i < sz; i++)
         {
-            if(nums[i] == 0)
-            {
-                count++;
-                if(count >= 2)
-                {
-                    ret.assign(nums.size(),0);
-                    return ret;
-                }
-            }
-            else
-            {
-                product *= nums[i];
-            }
+            productbefore[i] = productbefore[i-1] * nums[i-1];
+            productafter[sz-i-1] = productafter[sz-i] * nums[sz-i];
         }
-        if(count == 1)
+        for(int i = 0; i < sz; i++)
         {
-            for(int i = 0; i < nums.size(); i++)
-            {
-                if(nums[i] != 0)
-                {
-                    ret.push_back(0);
-                }
-                else
-                {
-                    ret.push_back(product);
-                }
-            }
-        }
-        else
-        {
-            for(int i = 0; i < nums.size(); i++)
-            {
-                ret.push_back(product / nums[i]);
-            }
+            ret[i] = productafter[i] * productbefore[i];
         }
         return ret;
     }
 };
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int product = 1;
+//         int count = 0;
+//         vector<int> ret;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(nums[i] == 0)
+//             {
+//                 count++;
+//                 if(count >= 2)
+//                 {
+//                     ret.assign(nums.size(),0);
+//                     return ret;
+//                 }
+//             }
+//             else
+//             {
+//                 product *= nums[i];
+//             }
+//         }
+//         if(count == 1)
+//         {
+//             for(int i = 0; i < nums.size(); i++)
+//             {
+//                 if(nums[i] != 0)
+//                 {
+//                     ret.push_back(0);
+//                 }
+//                 else
+//                 {
+//                     ret.push_back(product);
+//                 }
+//             }
+//         }
+//         else
+//         {
+//             for(int i = 0; i < nums.size(); i++)
+//             {
+//                 ret.push_back(product / nums[i]);
+//             }
+//         }
+//         return ret;
+//     }
+// };
