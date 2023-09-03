5d25e728a3ddaa220f1984d4bb45bf0c2d1b57f0
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-09-03 16:14:32 -0700

    Update 11. Container With Most Water.cpp

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0, j = height.size() - 1;
-        int Area = min(height[i], height[j]) * (j - i);
-        while(j > i)
+        int sz = height.size();
+        int left = 0;
+        int right = sz - 1;
+        int ret = 0;
+        while(right > left)
         {
-            if(height[i] < height[j])
-            {
-                i++;
-            }
+            ret = max(ret, min(height[right], height[left]) * (right - left));
+            if(height[left] < height[right])
+                left++;
             else
-            {
-                j--;
-            }
-            if(min(height[i], height[j]) * (j - i) > Area)
-                Area = min(height[i], height[j]) * (j - i);
+                right--;
         }
-        return Area;
+        return ret;
     }
 };
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int i = 0, j = height.size() - 1;
+//         int Area = min(height[i], height[j]) * (j - i);
+//         while(j > i)
+//         {
+//             if(height[i] < height[j])
+//             {
+//                 i++;
+//             }
+//             else
+//             {
+//                 j--;
+//             }
+//             if(min(height[i], height[j]) * (j - i) > Area)
+//                 Area = min(height[i], height[j]) * (j - i);
+//         }
+//         return Area;
+//     }
+// };
