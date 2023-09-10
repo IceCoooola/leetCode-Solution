1af0a6e891bb999a5ce83c8a02a18c9dbf7f52f5
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-09-09 18:33:54 -0700

    Update 9. Palindrome Number.cpp

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -3,22 +3,42 @@ public:
     bool isPalindrome(int x) {
         if(x < 0)
             return false;
-        vector<int> arr;
-        while(x)
+        long long rev = 0;
+        int tmp = x;
+        while(tmp)
         {
-            arr.push_back(x%10);
-            x/=10;
+            rev = ((rev * 10) + (tmp % 10));
+            tmp /= 10;
         }
-        int left = 0;
-        int right = arr.size() - 1;
-        while(right > left && arr[right] == arr[left])
-        {
-            ++left;
-            --right;
-        }
-        if(right <=left)
-        return true;
+        if(rev == x)
+            return true;
         else
-        return false;
+            return false;
+        
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if(x < 0)
+//             return false;
+//         vector<int> arr;
+//         while(x)
+//         {
+//             arr.push_back(x%10);
+//             x/=10;
+//         }
+//         int left = 0;
+//         int right = arr.size() - 1;
+//         while(right > left && arr[right] == arr[left])
+//         {
+//             ++left;
+//             --right;
+//         }
+//         if(right <=left)
+//         return true;
+//         else
+//         return false;
+//     }
+// };
