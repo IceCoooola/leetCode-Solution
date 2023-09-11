a7403b4c5c8deb5961e4f3a444c1257b3a80e3cb
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-09-10 17:09:34 -0700

    Update 155. Min Stack.cpp

--- 155. Min Stack.cpp
+++ 155. Min Stack.cpp
@@ -42,3 +42,40 @@ private:
     stack<int> st;
     vector<int> minVal;
 };
+
+
+// class MinStack {
+// public:
+//     MinStack() {
+//     }
+    
+//     void push(int val) {
+//         st.insert(val);
+//         data.push(val);
+//     }
+    
+//     void pop() {
+
+//         st.erase(st.find(data.top()));
+//         data.pop();
+//     }
+    
+//     int top() {
+//         return data.top();
+//     }
+    
+//     int getMin() {
+//         return *st.begin();
+//     }
+//     stack<int> data;
+//     multiset<int> st;
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
