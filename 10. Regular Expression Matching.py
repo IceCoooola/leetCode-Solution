f8e022831936cfe2608fe31442ab76040e58a61d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-12-05 16:28:37 -0800

    Update 10. Regular Expression Matching.py

--- 10. Regular Expression Matching.py
+++ 10. Regular Expression Matching.py
@@ -1,15 +1,22 @@
 class Solution:
+    matched = {}
     def isMatch(self, s: str, p: str) -> bool:
-        if len(s) == 0 and len(p) == 0:
-            return True
-        if len(s) == 0:
-            return (len(p) / 2 == p.count("*"))
-        if len(p) == 0:
-            return False
-        if len(p) > 1 and p[1] == '*':
+        print(s, p)
+        if (s, p) in self.matched:
+            return self.matched[(s, p)]
+        elif len(s) == 0 and len(p) == 0:
+            self.matched[(s, p)] = True
+        elif len(s) == 0:
+             self.matched[(s, p)] = len(p) / 2 == p.count("*")
+        elif len(p) == 0:
+            self.matched[(s, p)] = False
+        elif len(p) > 1 and p[1] == '*':
             if p[0] == s[0] or p[0] == '.':
-                return Solution.isMatch(self, s, p[2:]) or Solution.isMatch(self, s[1:], p)
-            return Solution.isMatch(self, s, p[2:])
-        if p[0] == '.' or s[0] == p[0]:
-            return Solution.isMatch(self, s[1:], p[1:])
-        return False
+                self.matched[(s, p)] = Solution.isMatch(self, s, p[2:]) or Solution.isMatch(self, s[1:], p)
+            else:
+                self.matched[(s, p)] = Solution.isMatch(self, s, p[2:])
+        elif p[0] == '.' or s[0] == p[0]:
+            self.matched[(s, p)] = Solution.isMatch(self, s[1:], p[1:])
+        elif p[0] != s[0]:
+            self.matched[(s, p)] = False
+        return self.matched[(s, p)]
