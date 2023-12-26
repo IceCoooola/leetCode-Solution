c560e54b4c316a9e22ed1bd3397519ba57387896
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-12-26 01:04:06 -0800

    Create 71. Simplify Path.py

---
+++ 71. Simplify Path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        arr = path.split('/')
+        ret = []
+        simplified_path = "/"
+        for p in arr:
+            if p == "..":
+                ret = ret[1:]
+            elif p != '':
+                ret.append(p)
+        for p in ret:
+            simplified_path += p
+            simplified_path += '/'
+        if len(simplified_path) > 1:
+            simplified_path = simplified_path[0:len(simplified_path) - 1]
+        return simplified_path
