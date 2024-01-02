eb05ff17b38b9f6d3171f3bab63e69a1de27b75a
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2024-01-02 13:55:04 -0800

    Update 15. 3Sum.cpp

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -13,8 +13,7 @@ public:
             int right = n - 1;
             while (left < right) {
                 if (nums[left] + nums[right] == sum) {
-                    vector<int> tmp = {nums[i], nums[left], nums[right]};
-                    ret.push_back(tmp);
+                    ret.push_back({nums[i], nums[left], nums[right]});
                     ++left;
                     --right;
                     while (left < n && nums[left] == nums[left - 1]) {
@@ -35,3 +34,41 @@ public:
         return ret;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> ret = vector<vector<int>>(); 
+//         int n = nums.size();
+//         for (int i = 0; i < n; ++i) {
+//             if (i > 0 && nums[i] == nums[i-1]) {
+//                 continue;
+//             }
+//             int sum = - nums[i];   
+//             int left = i + 1;
+//             int right = n - 1;
+//             while (left < right) {
+//                 if (nums[left] + nums[right] == sum) {
+//                     vector<int> tmp = {nums[i], nums[left], nums[right]};
+//                     ret.push_back(tmp);
+//                     ++left;
+//                     --right;
+//                     while (left < n && nums[left] == nums[left - 1]) {
+//                         ++left;
+//                     }
+//                     while (right > 0 && nums[right] == nums[right + 1]) {
+//                         --right;
+//                     }
+//                 }
+//                 else if (nums[left] + nums[right] < sum) {
+//                     ++left;
+//                 }
+//                 else {
+//                     --right;
+//                 }
+//             }
+//         }
+//         return ret;
+//     }
+// };
