440e64938f4af3422d54386aa36421fc57571e9d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2024-01-10 15:53:00 -0800

    Update 1143. Longest Common Subsequence.cpp

--- 1143. Longest Common Subsequence.cpp
+++ 1143. Longest Common Subsequence.cpp
@@ -13,10 +13,31 @@ public:
                     dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                    dp[i][j] = max(dp[i][j], dp[i-1][j-1]);
                 }
             }
         }
         return dp[m][n];
     }
 };
+// class Solution {
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         // dp[i][j] = t1[0:i] t2[0:j] 
+//         // t1[i] == t2[j]  -> dp[i-1][j-1]
+//         // t1[i] != t2[j] ->dp[i-1][j] dp[i][j-1] dp[i-1][j-1] max
+//         int m = text1.length();
+//         int n = text2.length();
+//         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+//         for (int i = 1; i < m + 1; ++i) {
+//             for (int j = 1; j < n + 1; ++j) {
+//                 if (text1[i-1] == text2[j-1]) {
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 } else {
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                     dp[i][j] = max(dp[i][j], dp[i-1][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
